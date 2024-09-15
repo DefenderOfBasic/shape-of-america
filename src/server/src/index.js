e03dcc7caac46389341e575b0f818913fbe8a938
DefenderOfBasic <defenderofbasic@proton.me>
2024-09-15 15:40:16 -0400

    add recount route

--- src/server/src/index.js
+++ src/server/src/index.js
@@ -18,7 +18,8 @@ const functionsForPaths = {
 	'/user-count': getUsersCount,
 	'/insert-guess': insertGuess,
 	'/global-accuracy': globalAccuracy,
-	'/global-tally': globalTally
+	'/global-tally': globalTally,
+	// '/recount': recount
 }
 
 const corsHeaders = {
@@ -38,6 +39,30 @@ export default {
 		return new Response('');
 	},
 };
+
+// async function recount(request, env, ctx) {
+// 	// re-update job_data from user guesses
+// 	let { results } = await env.DB.prepare(
+// 		`SELECT * FROM user_guesses`,
+// 	).all();
+
+// 	const user_guesses = results 
+
+// 	for (let item of user_guesses) {
+// 		const { guess, job_title } = item
+// 		const valuesMap = { 'democrat': '1, 0, 0', 'republican': '0, 1, 0', 'mixed': '0, 0, 1'}
+// 		await env.DB.prepare(`
+// 		INSERT INTO job_data (job_title, democrat, republican, mixed)
+// 		VALUES (?, ${valuesMap[guess]})
+// 		ON CONFLICT (job_title) 
+// 		DO UPDATE SET 
+// 			democrat = job_data.democrat + EXCLUDED.democrat,
+// 			republican = job_data.republican + EXCLUDED.republican,
+// 			mixed = job_data.mixed + EXCLUDED.mixed;`).bind( job_title ).run();
+// 	}
+
+// 	return new Response("done", { headers: corsHeaders })
+// }
 
 async function globalTally(request, env, ctx) {
 	const { results } = 
